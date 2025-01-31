fb4524ee528bafdc16e29f29f8760feee82f0689
Andrey <almakaevandrey@gmail.com>
2025-01-31 17:03:22 +0300

    fix the bug

---
+++ utils.js
@@ -0,0 +1,18 @@
+const TODOS_KEY = "todos";
+
+
+export const saveTodosIntoLocalStorage = (todos) => {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
+export const getTodosIntoLocalStorage = () => {
+    return JSON.parse(localStorage.getItem(TODOS_KEY)) || [];
+}
+
+export const getDateRepresentation = (todoCreatedDate) => {
+    return Intl.DateTimeFormat("ru-Ru", {
+        day: "numeric",
+        month: "numeric",
+        year: "numeric",
+    }).format(todoCreatedDate)
+}
